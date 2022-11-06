563e906b5dcab7d744f3a7f45afbe94b204b3f1f
ilomoyou <lsj_moon@163.com>
2022-11-06 21:49:44 +0800

    perf(laravel): 路由前缀分组

--- laravel/routes/wx.php
+++ laravel/routes/wx.php
@@ -6,24 +6,33 @@ use App\Http\Controllers\Wx\BrandController;
 use App\Http\Controllers\Wx\CatalogController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/register', [AuthController::class, 'register']); // 账号注册
-Route::post('auth/reg-captcha', [AuthController::class, 'regCaptcha']); // 注册验证码
-Route::post('auth/login', [AuthController::class, 'login']); // 账号登录
-Route::get('auth/info', [AuthController::class, 'info']); // 用户信息
-Route::post('auth/logout', [AuthController::class, 'logout']); // 账号退出
-Route::post('auth/reset', [AuthController::class, 'reset']); // 密码重置
-Route::post('auth/profile', [AuthController::class, 'profile']); // 账号信息修改
+# 用户模块-鉴权
+Route::prefix('auth')->group(function () {
+    Route::get('/info', [AuthController::class, 'info']); // 用户信息
+    Route::post('/register', [AuthController::class, 'register']); // 账号注册
+    Route::post('/reg-captcha', [AuthController::class, 'regCaptcha']); // 注册验证码
+    Route::post('/login', [AuthController::class, 'login']); // 账号登录
+    Route::post('/logout', [AuthController::class, 'logout']); // 账号退出
+    Route::post('/reset', [AuthController::class, 'reset']); // 密码重置
+    Route::post('/profile', [AuthController::class, 'profile']); // 账号信息修改
+});
 
 # 用户模块-地址
-Route::get('address/list', [AddressController::class, 'list']); // 收货地址列表
-Route::get('address/detail', [AddressController::class, 'detail']); // 收货地址详情
-Route::post('address/save', [AddressController::class, 'save']); // 保存收货地址
-Route::post('address/delete', [AddressController::class, 'delete']); // 删除收货地址
+Route::prefix('address')->group(function () {
+    Route::get('/list', [AddressController::class, 'list']); // 收货地址列表
+    Route::get('/detail', [AddressController::class, 'detail']); // 收货地址详情
+    Route::post('/save', [AddressController::class, 'save']); // 保存收货地址
+    Route::post('/delete', [AddressController::class, 'delete']); // 删除收货地址
+});
 
 # 商品模块-类目
-Route::get('/catalog/index', [CatalogController::class, 'index']); // 分类目录全部分类
-Route::get('/catalog/current', [CatalogController::class, 'current']); // 分类目录当前分类
+Route::prefix('catalog')->group(function () {
+    Route::get('/index', [CatalogController::class, 'index']); // 分类目录全部分类
+    Route::get('/current', [CatalogController::class, 'current']); // 分类目录当前分类
+});
 
 # 商品模块-品牌
-Route::get('brand/list', [BrandController::class, 'list']); // 品牌列表
-Route::get('brand/detail', [BrandController::class, 'detail']); // 品牌详情
+Route::prefix('brand')->group(function () {
+    Route::get('/list', [BrandController::class, 'list']); // 品牌列表
+    Route::get('/detail', [BrandController::class, 'detail']); // 品牌详情
+});
