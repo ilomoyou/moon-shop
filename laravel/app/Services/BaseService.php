98227b4c7685910df3976049d52caa712886b5a6
ilomoyou <lsj_moon@163.com>
2022-11-30 17:56:27 +0800

    perf(laravel): Service 单例模式优化

--- laravel/app/Services/BaseService.php
+++ laravel/app/Services/BaseService.php
@@ -4,17 +4,33 @@
 namespace App\Services;
 
 
+use Mockery;
+
 class BaseService
 {
-    protected static $instance;
+    protected static $instance = [];
 
-    // 服务层的单例模式
+    /**
+     * 服务层的单例模式
+     * @return mixed|static
+     */
     public static function getInstance()
     {
-        if (static::$instance instanceof static) {
-            return static::$instance;
+        if ((static::$instance[static::class] ?? null) instanceof static) {
+            return static::$instance[static::class];
         }
-        return static::$instance = new static();
+        return static::$instance[static::class] = new static();
+    }
+
+    /**
+     * 单测Mockery
+     * @return Mockery\Mock
+     */
+    public static function mockInstance()
+    {
+        return static::$instance[static::class] = Mockery::mock(static::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
     }
 
     // 私有化 __construct 和 __clone 方法 禁止外部实例化实现单例
